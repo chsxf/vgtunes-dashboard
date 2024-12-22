ad359a4af659845f8675d0fc36ea7c65aa01ff57
Christophe SAUVEUR <chsxf.pro@gmail.com>
2024-12-23 00:02:25 +0100

    ✨ Album search

--- routes/Albums.php
+++ routes/Albums.php
@@ -13,12 +13,31 @@ final class Albums extends BaseRouteProvider implements IPaginationProvider
 {
     private int $totalItemCountBuffer;
 
+    private readonly ?string $filteredQuery;
+
     public function __construct(protected readonly ICoreServiceProvider $serviceProvider)
     {
+        $query = trim($_REQUEST['q'] ?? '');
+        if (empty($query)) {
+            $this->filteredQuery = null;
+        } else {
+            $this->filteredQuery = $query;
+        }
+
+        $sql = "SELECT COUNT(`id`) FROM `albums`";
+        $values = [];
+        if ($this->filteredQuery !== null) {
+            $sql .= " WHERE `title` LIKE ?";
+            $values[] = "%{$this->filteredQuery}%";
+        }
+
         $dbService = $this->serviceProvider->getDatabaseService();
         $dbConn = $dbService->open();
-        $sql = "SELECT COUNT(`id`) FROM `albums`";
-        $this->totalItemCountBuffer = $dbConn->getValue($sql);
+        if (($queriedCount = $dbConn->getValue($sql, $values)) !== false) {
+            $this->totalItemCountBuffer = $queriedCount;
+        } else {
+            trigger_error('An error has occured while enumerating albums');
+        }
         $dbService->close($dbConn);
     }
 
@@ -35,7 +54,7 @@ final class Albums extends BaseRouteProvider implements IPaginationProvider
     #[Route, RequiredRequestMethod(RequestMethod::GET)]
     public function list(): RequestResult
     {
-        $pageManager = new PaginationManager($this);
+        $pageManager = new PaginationManager($this, ['q']);
 
         $dbService = $this->serviceProvider->getDatabaseService();
         $dbConn = $dbService->open();
@@ -43,10 +62,15 @@ final class Albums extends BaseRouteProvider implements IPaginationProvider
         $sql = "SELECT `al`.`id`, `al`.`title`, `ar`.`name` AS `artist_name`
                     FROM `albums` AS `al`
                     LEFT JOIN `artists` AS `ar`
-                        ON `ar`.`id` = `al`.`artist_id`
-                    ORDER BY `al`.`title` ASC";
+                        ON `ar`.`id` = `al`.`artist_id`";
+        $values = [];
+        if ($this->filteredQuery !== null) {
+            $sql .= " WHERE `al`.`title` LIKE ?";
+            $values[] = "%{$this->filteredQuery}%";
+        }
+        $sql .= " ORDER BY `al`.`title` ASC";
         $sql .= $pageManager->sqlLimit();
-        if (($albums = $dbConn->get($sql, \PDO::FETCH_ASSOC)) === false) {
+        if (($albums = $dbConn->get($sql, \PDO::FETCH_ASSOC, $values)) === false) {
             trigger_error('An error has occured while loading albums.', E_ERROR);
             return RequestResult::buildRedirectRequestResult('/');
         }
